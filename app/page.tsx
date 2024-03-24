5e9187a5612c0c7f4564b965100d1ea6a9778ee7
salume <salume.palizban@gmail.com>
2024-03-24 10:08:25 +0330

    out page from ssg

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import ProductCarousel from './_components/ProductCarousel';
 import ContactBar from './_components/index/ContactBar';
 import AboutGraniteSink from './_components/index/AboutGraniteSink';
 
+export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
+
 export default function Home() {
   return (
     <main className="min-h-screen">
